835b03e010a18ee15d0c58f4457d33c6b769a9a7
FabyBroc <fabybroc@hotmail.com>
2021-12-15 10:53:46 +0100

    Initial commit

---
+++ src/main/java/org/factoriaf5/libritos/controllers/BookController.java
@@ -0,0 +1,45 @@
+package org.factoriaf5.libritos.controllers;
+
+import org.factoriaf5.libritos.repositories.Book;
+import org.factoriaf5.libritos.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class BookController {
+
+    private BookRepository bookRepository;
+
+    @Autowired
+    public BookController(BookRepository bookRepository) {
+
+        this.bookRepository = bookRepository;
+    }
+
+    @GetMapping("/books")
+    String listBooks(Model model) {
+        List<Book> books = (List<Book>) bookRepository.findAll();
+        model.addAttribute("title", "Book list");
+        model.addAttribute("books", books);
+        return "books/all";
+    }
+
+    @GetMapping("/books/new")
+    String getForm(Model model){
+        Book book = new Book();
+        model.addAttribute("book", book);
+        return "books/new";
+    }
+
+    @PostMapping("/books/new")
+    String addBook(@ModelAttribute Book book) {
+        bookRepository.save(book);
+        return "redirect:/books";
+    }
+}
